cf64bdba27eea5bf2316d82e5ec53f29eaf0e809
Herbert Yao <grimreaperling@gmail.com>
2022-11-27 22:18:04 +0800

    Add the syntax highlight for the .y and .flex file.

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -1,2 +1,6 @@
 path_prepend "$HOME/.local/bin"
 path_prepend "$HOME/.dotfiles/bin"
+path_prepend "/root/pintos/tools/x86_64/bin"
+path_prepend "/root/pintos/tools/bochs/bin"
+path_prepend "/root/Cool/bin"
+
